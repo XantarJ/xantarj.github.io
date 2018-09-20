1942c1e7cbdd37b71295851bfef3cb12e9565a27
antonmry <anton@galiglobal.com>
2018-09-20 11:00:00 +0200

    Fixed countdown without weeks div

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,7 +61,7 @@ $(document).ready(function() {
     $(".countdown")
         .countdown("2018/09/29", function(event) {
             $(this).html(
-                event.strftime('<div>%w <span>Weeks</span></div>  <div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
+                event.strftime('<div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
             );
         });
 
